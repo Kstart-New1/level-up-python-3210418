d1b71075f4039379ca33869512e8e832e6dd2677
Kstart-New1 <147348775+Kstart-New1@users.noreply.github.com>
2024-03-28 15:37:04 +0000

    solve equation

--- src/05 Play the Waiting Game/waiting_game.py
+++ src/05 Play the Waiting Game/waiting_game.py
@@ -1,16 +1,12 @@
 import time
 import random
-
 def waiting_game():
     target = random.randint(2, 4)  # target seconds to wait
     print(f'\nYour target time is {target} seconds')
-
     input(' ---Press Enter to Begin--- ')
     start = time.perf_counter()
-
     input(f'\n...Press Enter again after {target} seconds...')
     elapsed = time.perf_counter() - start
-
     print(f'\nElapsed time: {elapsed :.3f} seconds')
     if elapsed == target:
         print('(Unbelievable! Perfect timing!)')
@@ -19,7 +15,7 @@ def waiting_game():
     else:
         print(f'({elapsed - target :.3f} seconds too slow)')
 
-
 # commands used in solution video for reference
 if __name__ == '__main__':
     waiting_game()
+
